c8e06ee1f4ebf95b2a4c6062b596948a965c8343
Jeremy E Kozdon <jeremy.kozdon@nextsilicon.com>
2023-12-19 17:16:04 +0000

    vim
    Signed-off-by: Jeremy E Kozdon <jeremy.kozdon@nextsilicon.com>

--- nvim/init.lua
+++ nvim/init.lua
@@ -326,8 +326,9 @@ vim.o.termguicolors = true
 vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 
 -- Remap for dealing with word wrap
-vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
-vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+-- vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+-- vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+   vim.o.wrap = false
 
 -- [[ Highlight on yank ]]
 -- See `:help vim.highlight.on_yank()`
