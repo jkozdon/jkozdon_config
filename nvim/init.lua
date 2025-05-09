dd32daf77fd8d1bbfa1348b5200185a4bb06b58d
Jeremy E Kozdon <jeremy@kozdon.net>
2025-05-08 22:12:29 -0700

    branch

--- nvim/init.lua
+++ nvim/init.lua
@@ -190,6 +190,7 @@ require('lazy').setup({
   },
   {
     "CopilotC-Nvim/CopilotChat.nvim",
+    branch = "main",
     dependencies = {
       { "github/copilot.vim" },
       { "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
