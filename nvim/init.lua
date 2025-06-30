03a5286b59f2e1d0f24df89b01b70f81f589abfb
Jeremy E Kozdon <jeremy.kozdon@nextsilicon.com>
2025-07-01 02:06:47 +0300

    update neovim init
    Signed-off-by: Jeremy E Kozdon <jeremy.kozdon@nextsilicon.com>

--- nvim/init.lua
+++ nvim/init.lua
@@ -277,16 +277,16 @@ require('lazy').setup({
       "MeanderingProgrammer/render-markdown.nvim",
       ft = { "markdown", "codecompanion" }
     },
-    {
-      "OXY2DEV/markview.nvim",
-      lazy = false,
-      opts = {
-        preview = {
-          filetypes = { "markdown", "codecompanion" },
-          ignore_buftypes = {},
-        },
-      },
-    },
+    -- {
+    --   "OXY2DEV/markview.nvim",
+    --   lazy = false,
+    --   opts = {
+    --     preview = {
+    --       filetypes = { "markdown", "codecompanion" },
+    --       ignore_buftypes = {},
+    --     },
+    --   },
+    -- },
     {
       "echasnovski/mini.diff",
       config = function()
@@ -496,6 +496,17 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   group = highlight_group,
   pattern = '*',
 })
+
+vim.api.nvim_create_autocmd('LspAttach', {
+  callback = function(ev)
+    local client = vim.lsp.get_client_by_id(ev.data.client_id)
+    if client:supports_method('textDocument/completion') then
+      vim.lsp.completion.enable(true, client.id, ev.buf, {autotrigger = true})
+    end
+  end,
+})
+
+vim.diagnostic.config({virtual_lines = { current_line = true }})
 
 -- [[ Configure Telescope ]]
 -- See `:help telescope` and `:help telescope.setup()`
